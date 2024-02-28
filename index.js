5108ee0866a39d2031167ffa34cd3619b2314dd0
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2024-02-29 02:49:49 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+// returnFirstTwoDrivers
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+
+// returnLastTwoDrivers
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+
+// selectingDrivers
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// createFareMultiplier
+const createFareMultiplier = function (integer) {
+  return function (fare) {
+    return fare * integer;
+  };
+};
+
+// fareDoubler
+const fareDoubler = createFareMultiplier(2);
+
+// fareTripler
+const fareTripler = createFareMultiplier(3);
+
+// selectDifferentDrivers
+const selectDifferentDrivers = function (drivers, returnDriversFunction) {
+  return returnDriversFunction(drivers);
+};
+
+// Don't forget to export if you're using this in another file
+module.exports = {
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers,
+  selectingDrivers,
+  createFareMultiplier,
+  fareDoubler,
+  fareTripler,
+  selectDifferentDrivers,
+};
